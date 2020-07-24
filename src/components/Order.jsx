05e938209bec085cac73d627afbf9635d634179d
roboblox <supeecyborg@gmail.com>
2020-07-24 14:29:13 -0400

    form tweaked

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,25 +1,27 @@
 import React from "react";
+import { useRouteMatch } from "react-router-dom";
 
 export default function Order({ order }) {
   if (!order) {
     return <h3>Working fetching your order...</h3>;
   }
 
+  const toppingArr = Object.keys(order.toppings);
+
   return (
     <div className="order container">
       <h2>{order.name}</h2>
       <p>Size: {order.size}</p>
       <p>Special Instructions: {order.instructions}</p>
-      {order.toppings && order.toppings.length && (
-        <div>
-          Toppings:
-          <ul>
-            {order.toppings.map((like, idx) => (
-              <li key={idx}>{like}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+
+      <div>
+        Toppings:
+        <ul>
+          {toppingArr.map((topping, id) => (
+            <li key={id}>{topping}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
